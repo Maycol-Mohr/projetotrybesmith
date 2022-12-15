90f3442a3ea440dc1b9529f29fbc4cd533582b3b
Maycol-Mohr <maycolmohr@yahoo.com.br>
2022-12-15 16:26:44 +0100

    tentando exercicio 7 com ajuda

---
+++ src/middlewares/userCreate7Middleware.ts
@@ -0,0 +1,122 @@
+// import { NextFunction, Request, Response } from 'express';
+
+// import { User } from '../interfaces';
+
+// function validateItemsString(username: string, vocation: string, level: number, password: string) {
+//   if (typeof username !== 'string') {
+//     const message = '"username" must be a string';
+//     return ({ status: 422, message });
+//   }
+//   if (typeof vocation !== 'string') {
+//     const message = '"vocation" must be a string';
+//     return ({ status: 422, message });
+//   }
+//   if (typeof level !== 'number') {
+//     const message = '"level" must be a number';
+//     return ({ status: 422, message });
+//   }
+//   if (typeof password !== 'string') {
+//     const message = '"password" must be a string';
+//     return ({ status: 422, message });
+//   }
+//   return null;
+// }
+
+// function validateItemsMinimun(username: string, vocation: string, level: number, password: string) {
+//   if (username.length < 3) {
+//     const message = '"username" length must be at least 3 characters long';
+//     return ({ status: 422, message });
+//   }
+    
+//   if (vocation.length < 3) {
+//     const message = '"vocation" length must be at least 3 characters long';
+//     return ({ status: 422, message });
+//   }
+//   if (Number(level) < 1) {
+//     const message = '"level" must be greater than or equal to 1';
+//     return ({ status: 422, message });
+//   }
+//   if (password.length < 8) {
+//     const message = '"password" length must be at least 8 characters long';
+//     return ({ status: 422, message });
+//   }
+    
+//   return null;
+// }
+
+// // function validateIsRequired(username: string, vocation: string, level: number, password: string) {
+// //   if (!username) {
+// //     const message = '"username" is required';
+// //     return ({ status: 400, message });
+// //   }
+// //   if (!vocation) {
+// //     const message = '"vocation" is required';
+// //     return ({ status: 400, message });
+// //   }
+// //   if (!level) {
+// //     const message = '"level" is required';
+// //     return ({ status: 400, message });
+// //   }
+// //   if (!password) {
+// //     const message = '"password" is required';
+// //     return ({ status: 400, message });
+// //   }
+      
+// //   return null;
+// // }
+
+// export default function validateRequired(req: Request, res: Response, next: NextFunction) {
+//   const { username, vocation, level, password } = req.body as User;
+//   if (!username) {
+//     const message = '"username" is required';
+//     return res.status(400).json({ message });
+//   }
+//   if (!vocation) {
+//     const message = '"vocation" is required';
+//     return res.status(400).json({ message });
+//   }
+//   if (!level) {
+//     const message = '"level" is required';
+//     return res.status(400).json({ message });
+//   }
+//   if (!password) {
+//     const message = '"password" is required';
+//     return res.status(400).json({ message });
+//   }
+  
+//   let error = validateItemsString(username, vocation, level, password);
+  
+//   if (error) return res.status(error.status).json({ message: error.message });
+  
+//   error = validateItemsMinimun(username, vocation, level, password);
+  
+//   if (error) return res.status(error.status).json({ message: error.message });
+
+//   //   error = validateIsRequired(username, vocation, level, password);
+  
+//   //   if (error) return res.status(error.status).json({ message: error.message });
+  
+//   next();
+// }
+
+import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
+
+const productSchema = Joi.object(
+  {
+    username: Joi.string().min(3).required(),
+    vocation: Joi.string().min(3).required(),
+    level: Joi.number().min(1).required(),
+    password: Joi.string().min(8).required(),
+  },
+);
+const validateProduct = (req: Request, res: Response, next: NextFunction) => {
+  const { error } = productSchema.validate(req.body);
+  if (error) {
+    const [details] = error.details;
+    const statusCode = details.type === 'any.required' ? 400 : 422; 
+    return res.status(statusCode).json({ message: error.message });
+  }
+  next();
+};
+export default validateProduct;
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import * as productController from '../controllers/productController';
 import userMiddleware from '../middlewares/userMiddleware';
 import productMiddleware from '../middlewares/productMiddleware';
+import userCreate7Middleware from '../middlewares/userCreate7Middleware';
 
 const router = Router();
 
@@ -10,6 +11,6 @@ router.get('/products', productController.getAll);
 router.get('/orders', productController.getAllOrders);
 router.post('/login', userMiddleware, productController.login);
 router.post('/products', productMiddleware, productController.createProduct);
-router.post('/users', productController.createUser);
+router.post('/users', userCreate7Middleware, productController.createUser);
 
 export default router;
